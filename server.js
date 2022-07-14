954eb4c59ef38bc680a4a915b1fb3cf0aa6bff0b
robester0403 <robkso@gmail.com>
2022-07-14 14:56:13 -0400

    Created and connected mongoDB, add listen and connected server to db. Installed Nodemon

--- server.js
+++ server.js
@@ -10,4 +10,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 require("dotenv").config();
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 5001;
+
+mongoose
+  .connect(process.env.ATLAS_URI)
+  .then(() => {
+    app.listen(5001);
+    console.log(`Connected to Port ${port}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
